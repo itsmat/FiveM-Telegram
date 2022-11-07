475630dcb94847dbef88ad88a39aa969a21556c3
Mat <79325182+itsmat@users.noreply.github.com>
2022-11-07 22:39:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pyrogram import Client 
 
 
+##### CONFIG #####
+log = False #put True if you want to receive the logs or put False if you don't want to receive them
+logdiscordwebhook = 'https://discord.com/api/webhooks/123/abc' #discord webhook
 ip = 'localhost:30120' #if localhost write localhost:port else ip:port
 app = Client(
     './sessioni/sessione', 
@@ -10,6 +13,8 @@ app = Client(
     plugins = dict(root="plugins")
 )
 
+
+##### RUNNER #####
 if __name__ == "__main__":
     app.run(print('Telegram Bot Started || github.com/itsmat - Mat#3616'))
 
