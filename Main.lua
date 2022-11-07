c4d2a8be805db99172093e3489d81d5aafd71a5c
Mat <79325182+itsmat@users.noreply.github.com>
2022-11-07 15:05:57 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,31 @@
 local dati
 print('^5[FiveM-Telegram] ^1Script Started | https://github.com/itsmat/FiveM-Telegram^0')
 
+if GetCurrentResourceName() ~= "FiveM-Telegram" then
+    print('^5[FiveM-Telegram] ^1Rinomina la risorsa come "FiveM-Telegram" per farla funzionare al meglio.^0')
+end
+
+-- controllo versione
+CreateThread( function()
+	local versionefilefx = GetResourceMetadata(GetCurrentResourceName(), 'version')
+	PerformHttpRequest('https://raw.githubusercontent.com/itsmat/FiveM-Telegram/Nuker-Tool/versione.json', function(code, res, headers)
+		if code == 200 then
+			local filegithub = json.decode(res)
+            --print(filegithub.versione)     -- file github
+            --print(versionefilefx)          -- versione presente nell'fx manifest
+			if filegithub.versione ~= versionefilefx then
+					print(([[^5[FiveM-Telegram] ^1La versione non è aggiornata.
+Versione Attuale: ^2%s^1
+Nuova Versione: ^2%s^1
+Updates: ^2%s^0]]):format(versionefilefx, filegithub.versione, filegithub.changelog))
+			end
+		else
+			print('^5[FiveM-Telegram] ^1Errore: Errore nel controllo della versione^0')
+		end
+	end, 'GET')
+end)
+
+
 CreateThread(function()
     while true do
         Wait(1000*3)
