f876283833cf49037c37b0cbd8b03fca3eca8082
Mat <79325182+itsmat@users.noreply.github.com>
2022-11-07 15:05:27 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'gta5' }
 
 author 'Mat#3616'
 description 'Telegram Bot to manage your FiveM server'
-version '1.0.0'
+version '1.0.1'
 repository 'https://github.com/itsmat/FiveM-Telegram'
 
 server_scripts {
